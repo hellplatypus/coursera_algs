6173a6657c442b8bf70571e942765b957770c5e9
hellplatypus <kirill.kreshchenko@gmail.com>
2016-03-10 17:30:17 +0200

    some refactoring

--- union-find.py
+++ union-find.py
@@ -2,7 +2,6 @@ class UnionFind:
     def __init__(self, n):
         self.arr = [i for i in range(n)]
         self.arr_size = [1] * n
-        self.largest_el_list = self.arr[:]
 
     def connected(self, p, q):
         return self._root(p) == self._root(q)
@@ -12,6 +11,26 @@ class UnionFind:
             # self.arr[node] = self.arr[self.arr[node]]
             node = self.arr[node]
         return node
+
+    def union(self, p, q):
+        i = self._root(p)
+        j = self._root(q)
+
+        if i == j:
+            return
+
+        if self.arr_size[i] < self.arr_size[j]:
+            self.arr[i] = j
+            self.arr_size[j] += self.arr_size[i]
+        else:
+            self.arr[j] = i
+            self.arr_size[i] += self.arr_size[j]
+
+
+class UnionFindWithSpecificCanonicalElement(UnionFind):
+    def __init__(self, n):
+        UnionFind.__init__(self, n)
+        self.largest_el_list = self.arr[:]
 
     def union(self, p, q):
         i = self._root(p)
@@ -36,16 +55,35 @@ class UnionFind:
         return self.largest_el_list[self._root(i)]
 
 
-uf = UnionFind(10)
+if __name__ == '__main__':
 
-uf.union(0, 4)
-uf.union(5, 4)
-uf.union(5, 3)
-uf.union(1, 2)
-uf.union(1, 7)
-uf.union(7, 4)
+    print 'Union-find: '
+    print '-------------------------------------------'
+    uf = UnionFind(10)
 
-print uf.find(3)
+    uf.union(0, 4)
+    uf.union(5, 4)
+    uf.union(5, 3)
+    uf.union(1, 2)
+    uf.union(1, 7)
+    uf.union(7, 4)
 
-print uf.arr
-print uf.arr_size
+    print uf.arr
+    print uf.arr_size
+
+    print 'Union Find With Specific Canonical Element: '
+    print '-------------------------------------------'
+
+    uf = UnionFindWithSpecificCanonicalElement(10)
+
+    uf.union(0, 4)
+    uf.union(5, 4)
+    uf.union(5, 3)
+    uf.union(1, 2)
+    uf.union(1, 7)
+    uf.union(7, 4)
+
+    print uf.find(3)
+
+    print uf.arr
+    print uf.arr_size
