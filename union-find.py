ef798b095b4c2f4e6ee7b07643c2413d4223440e
hellplatypus <kirill.kreshchenko@gmail.com>
2016-03-16 11:28:39 +0200

    wighted by height union find

--- union-find.py
+++ union-find.py
@@ -1,14 +1,14 @@
 class UnionFind:
     def __init__(self, n):
         self.arr = [i for i in range(n)]
-        self.arr_size = [1] * n
+        self.set_size = [1] * n
 
     def connected(self, p, q):
         return self._root(p) == self._root(q)
 
     def _root(self, node):
         while self.arr[node] != node:
-            # self.arr[node] = self.arr[self.arr[node]]
+            self.arr[node] = self.arr[self.arr[node]]
             node = self.arr[node]
         return node
 
@@ -19,12 +19,12 @@ class UnionFind:
         if i == j:
             return
 
-        if self.arr_size[i] < self.arr_size[j]:
+        if self.set_size[i] < self.set_size[j]:
             self.arr[i] = j
-            self.arr_size[j] += self.arr_size[i]
+            self.set_size[j] += self.set_size[i]
         else:
             self.arr[j] = i
-            self.arr_size[i] += self.arr_size[j]
+            self.set_size[i] += self.set_size[j]
 
 
 class UnionFindWithSpecificCanonicalElement(UnionFind):
@@ -39,25 +39,47 @@ class UnionFindWithSpecificCanonicalElement(UnionFind):
         if i == j:
             return
 
+        # processing the largest element in new merged set
         if self.largest_el_list[i] > self.largest_el_list[j]:
             self.largest_el_list[j] = self.largest_el_list[i]
         else:
             self.largest_el_list[i] = self.largest_el_list[j]
 
-        if self.arr_size[i] < self.arr_size[j]:
+        if self.set_size[i] < self.set_size[j]:
             self.arr[i] = j
-            self.arr_size[j] += self.arr_size[i]
+            self.set_size[j] += self.set_size[i]
         else:
             self.arr[j] = i
-            self.arr_size[i] += self.arr_size[j]
+            self.set_size[i] += self.set_size[j]
 
     def find(self, i):
         return self.largest_el_list[self._root(i)]
 
 
+class UnionFindWithTreeHeight(UnionFind):
+    def __init__(self, n):
+        UnionFind.__init__(self, n)
+        self.subtrees_height = [1] * n
+
+    def union(self, p, q):
+        i = self._root(p)
+        j = self._root(q)
+
+        if i == j:
+            return
+
+        if self.subtrees_height[i] == self.subtrees_height[j]:
+            self.subtrees_height[j] += 1
+            self.arr[i] = j
+        elif self.subtrees_height[i] < self.subtrees_height[j]:
+            self.arr[i] = j
+        else:
+            self.arr[j] = i
+
+
 if __name__ == '__main__':
 
-    print 'Union-find: '
+    print '\nUnion-find: '
     print '-------------------------------------------'
     uf = UnionFind(10)
 
@@ -69,9 +91,9 @@ if __name__ == '__main__':
     uf.union(7, 4)
 
     print uf.arr
-    print uf.arr_size
+    print uf.set_size
 
-    print 'Union Find With Specific Canonical Element: '
+    print '\nUnion Find With Specific Canonical Element: '
     print '-------------------------------------------'
 
     uf = UnionFindWithSpecificCanonicalElement(10)
@@ -86,4 +108,19 @@ if __name__ == '__main__':
     print uf.find(3)
 
     print uf.arr
-    print uf.arr_size
+    print uf.set_size
+
+    print '\nWeighted By Tree\'s Height Union Find: '
+    print '-------------------------------------------'
+
+    uf = UnionFindWithTreeHeight(10)
+
+    uf.union(0, 4)
+    uf.union(5, 4)
+    uf.union(5, 3)
+    uf.union(1, 2)
+    uf.union(1, 7)
+    uf.union(7, 4)
+
+    print uf.arr
+    print uf.subtrees_height
